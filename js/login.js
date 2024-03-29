de16fd105ebeac37ff983150cbadb31ba999e016
oooohri <127761560+oooohri@users.noreply.github.com>
2024-03-29 10:45:07 +0900

    fix : login.js 유효성검사 수정

--- js/login.js
+++ js/login.js
@@ -41,51 +41,49 @@ function myFunction() {
     });
 
   if (id === 'admin' && password === '123') {
-    if (id === 'admin' && password === '123') {
-      Swal.fire({
-        title: '로그인 되었습니다!',
-        icon: 'success',
-      });
-      window.location.assign('/sesac_project_eteam/html/mypage.html');
-    } else {
-      alert('아이디나 비밀번호를 확인하세요!');
-    }
+    Swal.fire({
+      title: '로그인 되었습니다!',
+      icon: 'success',
+    });
+    window.location.assign('/sesac_project_eteam/html/mypage.html');
+  } else {
+    alert('아이디나 비밀번호를 확인하세요!');
   }
-
-  function checkNum(event) {
-    var key = event.key;
-    console.log(key);
-    if (key.trim() == !'' || key.includes(' ')) {
-      alert('잘 못 입력했습니다');
-    }
-    if (event.target.value === ' ') {
-      idIput.value = '';
-    }
-  }
-  // // 맨 앞과 뒤의 공백을 제거한 후 빈 값인지 확인
-  // if (id.trim() === '') {
-  //   return false; // 빈 값인 경우
-  // } else {
-  //   id.preventDefault;
-  // }
-
-  // // 공백이 포함되어 있는지 확인
-  // if (id.includes(' ')) {
-  //   return false; // 공백이 포함된 경우
-  // }
-
-  // // 영어와 숫자 두 가지 타입이 모두 포함되었는지 확인
-  // let hasLetter = /[a-zA-Z]/.test(id); // 영어 확인
-  // let hasNumber = /\d/.test(id); // 숫자 확인
-  // if (!(hasLetter && hasNumber)) {
-  //   return false; // 영어와 숫자 두 가지 타입 중 하나라도 포함되지 않은 경우
-  // }
-
-  // // 한글이 포함되어 있는지 확인
-  // let hasKorean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(id);
-  // if (hasKorean) {
-  //   return false; // 한글이 포함된 경우
-  // }
-
-  // return true; // 모든 조건을 통과한 경우 유효한 아이디
 }
+
+function checkNum(event) {
+  var key = event.key;
+  console.log(key);
+  if (key.trim() == !'' || key.includes(' ')) {
+    alert('잘 못 입력했습니다');
+  }
+  if (event.target.value === ' ') {
+    idIput.value = '';
+  }
+}
+// // 맨 앞과 뒤의 공백을 제거한 후 빈 값인지 확인
+// if (id.trim() === '') {
+//   return false; // 빈 값인 경우
+// } else {
+//   id.preventDefault;
+// }
+
+// // 공백이 포함되어 있는지 확인
+// if (id.includes(' ')) {
+//   return false; // 공백이 포함된 경우
+// }
+
+// // 영어와 숫자 두 가지 타입이 모두 포함되었는지 확인
+// let hasLetter = /[a-zA-Z]/.test(id); // 영어 확인
+// let hasNumber = /\d/.test(id); // 숫자 확인
+// if (!(hasLetter && hasNumber)) {
+//   return false; // 영어와 숫자 두 가지 타입 중 하나라도 포함되지 않은 경우
+// }
+
+// // 한글이 포함되어 있는지 확인
+// let hasKorean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(id);
+// if (hasKorean) {
+//   return false; // 한글이 포함된 경우
+// }
+
+// return true; // 모든 조건을 통과한 경우 유효한 아이디
